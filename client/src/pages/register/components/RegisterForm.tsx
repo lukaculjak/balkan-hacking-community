b6ae3ce3240855567c2f8330d47819dd6f2920ab
lukaculjak <lukaculjak.se@gmail.com>
2023-12-27 22:43:00 +0100

    user sign up, login and reseting forgotten password

--- client/src/pages/register/components/RegisterForm.tsx
+++ client/src/pages/register/components/RegisterForm.tsx
@@ -5,10 +5,10 @@ import Form from "../../../ui/Form";
 function RegisterForm() {
   return (
     <Form>
+      <Input type="password" placeholder="username:"></Input>
       <Input type="text" placeholder="e-mail:"></Input>
       <Input type="password" placeholder="password:"></Input>
       <Input type="password" placeholder="repeat password:"></Input>
-      <Input type="password" placeholder="username:"></Input>
 
       <Button variant="primary">Create Account</Button>
     </Form>
